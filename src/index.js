b79f09628597e76fbc48f82953ace0082c20e970
Zak Klinedinst <111690550+RedNoodle325@users.noreply.github.com>
2023-02-25 05:21:55 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // it means same directory
+import 'tachyons';
+import App from './Containers/App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+serviceWorker.unregister();
