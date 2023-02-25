cad209db2d54b325f772e52834efedb92cf6689e
Zak Klinedinst <111690550+RedNoodle325@users.noreply.github.com>
2023-02-25 06:40:29 -0500

    Add files via upload

--- src/Components/Scroll.js
+++ src/Components/Scroll.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const Scroll = (props) => {
-return (
-	<div style={{overflowY: 'scroll',border: '0px solid black',height: '500px'}}>
-		{props.children}
-	</div>
-	);
+  return (
+    <div
+      style={{
+        overflowY: "scroll",
+        height: "500px",
+      }}
+    >
+      {props.children}
+    </div>
+  );
 };
- export default Scroll;
+
+export default Scroll;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css'; // it means same directory
-import 'tachyons';
-import App from './Containers/App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css"; // it means same directory
+import "tachyons";
+import App from "./Containers/App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 serviceWorker.unregister();
+
